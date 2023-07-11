ee9ea747bb53ef54f7b30a2814d7ee617aa924b2
Giacomo <reddy.depa@gmail.com>
2023-07-11 17:40:28 +0200

    badge js update

--- js/app_brand-guide.js
+++ js/app_brand-guide.js
@@ -1,3 +1,11 @@
 $( document ).ready(function() {
   var clipboard = new Clipboard('.clipboard');
 });
+
+  clipboard.on('success', function(e) {
+    $(e.trigger).text("Copied!");
+    e.clearSelection();
+    setTimeout(function() {
+      $(e.trigger).text("Copy");
+    }, 2500);
+  });
