1229e14b55e39696ea49723a21961d4a2bd5ede9
Giacomo <reddy.depa@gmail.com>
2023-07-11 17:57:15 +0200

    js update

--- js/app_brand-guide.js
+++ js/app_brand-guide.js
@@ -2,10 +2,12 @@ $( document ).ready(function() {
   var clipboard = new Clipboard('.clipboard');
 });
 
-clipboard.on('success', function(e) {
+$( document ).ready(function() {
+  clipboard.on('success', function(e) {
     $(e.trigger).text("Copied!");
     e.clearSelection();
     setTimeout(function() {
       $(e.trigger).text("Copy");
     }, 2500);
   });
+})
