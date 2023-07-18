5d6996d98399b7ac340e8a61e24684cb35cde28f
Giacomo <reddy.depa@gmail.com>
2023-07-18 11:47:37 +0200

    content, css and js updates

--- js/app_brand-guide.js
+++ js/app_brand-guide.js
@@ -82,3 +82,27 @@ $(document).ready(function(){
   document.getElementById("size_select-fgs").selectedIndex = "6";
   
 });
+
+
+// Dropdown content selection
+
+$(document).ready(function(){
+
+  //hides dropdown content
+  $(".size_chart-system").hide();
+  
+  //unhides first option content
+  $("#option1").show();
+  
+  
+  //listen to dropdown for change
+  $("#size_select-system").change(function(){
+    //rehide content on change
+    $('.size_chart').hide();
+    //unhides current item
+    $('#'+$(this).val()).show();
+  });
+
+  document.getElementById("size_select-system").selectedIndex = "0";
+  
+});
